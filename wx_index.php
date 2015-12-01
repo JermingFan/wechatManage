ae57f804040cfe6744307ce3a8875791954aa91e
Fancy <jemringfan@foxmail.com>
2015-12-01 10:51:22 +0800

    test

--- wx_index.php
+++ wx_index.php
@@ -6,7 +6,7 @@
 //define your token
 define("TOKEN", "weixin");
 $wechatObj = new wechatCallbackapiTest();
-$wechatObj->valid();
+$wechatObj->responseMsg();
 
 class wechatCallbackapiTest
 {
@@ -43,7 +43,7 @@ class wechatCallbackapiTest
 							<MsgType><![CDATA[%s]]></MsgType>
 							<Content><![CDATA[%s]]></Content>
 							<FuncFlag>0</FuncFlag>
-							</xml>";             
+							</xml>";
 				if(!empty( $keyword ))
                 {
               		$msgType = "text";
@@ -59,25 +59,25 @@ class wechatCallbackapiTest
         	exit;
         }
     }
-		
+
 	private function checkSignature()
 	{
         // you must define TOKEN by yourself
         if (!defined("TOKEN")) {
             throw new Exception('TOKEN is not defined!');
         }
-        
+
         $signature = $_GET["signature"];
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
-        		
+
 		$token = TOKEN;
 		$tmpArr = array($token, $timestamp, $nonce);
         // use SORT_STRING rule
 		sort($tmpArr, SORT_STRING);
 		$tmpStr = implode( $tmpArr );
 		$tmpStr = sha1( $tmpStr );
-		
+
 		if( $tmpStr == $signature ){
 			return true;
 		}else{
