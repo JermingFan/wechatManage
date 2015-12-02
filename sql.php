c7d338ad15b5da796f3d112c85218623eab6d7bd
Fancy <jemringfan@foxmail.com>
2015-12-02 15:09:32 +0800

    绑定

--- sql.php
+++ sql.php
@@ -1,7 +1,7 @@
 <?php
 
 // 连主库
-$link=mysqli_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
+$link=mysql_connect(SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
 
 // 连从库
 // $link=mysql_connect(SAE_MYSQL_HOST_S.':'.SAE_MYSQL_PORT,SAE_MYSQL_USER,SAE_MYSQL_PASS);
