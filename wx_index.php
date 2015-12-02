cd18f5bd4cc7724c1d06b9ecf23852dfb107ef0f
Fancy <jemringfan@foxmail.com>
2015-12-02 14:10:08 +0800

    二级菜单.php

--- wx_index.php
+++ wx_index.php
@@ -7,34 +7,35 @@ $wechatObj->responseMsg();
 
 class wechatCallbackapiTest
 {
-    public function valid()
-    {
-        $echoStr = $_GET["echostr"];
+	public function valid()
+	{
+		$echoStr = $_GET["echostr"];
 
-        //valid signature , option
-        if($this->checkSignature()){
-            echo $echoStr;
-            exit;
-        }
-    }
+		//valid signature , option
+		if($this->checkSignature()){
+			echo $echoStr;
+			exit;
+		}
+	}
 
-    public function responseMsg()
-    {
-        //get post data, May be due to the different environments
-        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+	public function responseMsg()
+	{
+		//get post data, May be due to the different environments
+		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
-        //extract post data
-        if (!empty($postStr)){
-            /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
-               the best way is to check the validity of xml by yourself */
-            libxml_disable_entity_loader(true);
-            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-            $fromUsername = $postObj->FromUserName;
-            $toUsername = $postObj->ToUserName;
-            $keyword = trim($postObj->Content);
-            $time = time();
-            $event = $postObj->Event;
-            $textTpl = "<xml>
+		//extract post data
+		if (!empty($postStr))
+		{
+			/* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+			   the best way is to check the validity of xml by yourself */
+			libxml_disable_entity_loader(true);
+			$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+			$fromUsername = $postObj->FromUserName;
+			$toUsername = $postObj->ToUserName;
+			$keyword = trim($postObj->Content);
+			$time = time();
+			$event = $postObj->Event;
+			$textTpl = "<xml>
 							<ToUserName><![CDATA[%s]]></ToUserName>
 							<FromUserName><![CDATA[%s]]></FromUserName>
 							<CreateTime>%s</CreateTime>
@@ -42,80 +43,95 @@ class wechatCallbackapiTest
 							<Content><![CDATA[%s]]></Content>
 							<FuncFlag>0</FuncFlag>
 							</xml>";
-            $imageTpl = "<xml>
-                            <ToUserName><![CDATA[%s]]></ToUserName>
-                            <FromUserName><![CDATA[%s]]></FromUserName>
-                            <CreateTime>%s</CreateTime>
-                            <MsgType><![CDATA[news]]></MsgType>//消息类型为news（图文）
-                            <ArticleCount>1</ArticleCount>//图文数量为1（单图文）
-                            <Articles>
-                            <item>//第一张图文消息
-                            <Title><![CDATA[%s]]></Title> //标题
-                            <Description><![CDATA[%s]]></Description>//描述
-                            <PicUrl><![CDATA[%s]]></PicUrl>//打开前的图片链接地址
-                            <Url><![CDATA[%s]]></Url>//点击进入后显示的图片链接地址
-                            </item>
-                            </Articles>
-                            </xml> ";
+			$imageTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[news]]></MsgType>//消息类型为news（图文）
+							<ArticleCount>1</ArticleCount>//图文数量为1（单图文）
+							<Articles>
+							<item>//第一张图文消息
+							<Title><![CDATA[%s]]></Title> //标题
+							<Description><![CDATA[%s]]></Description>//描述
+							<PicUrl><![CDATA[%s]]></PicUrl>//打开前的图片链接地址
+							<Url><![CDATA[%s]]></Url>//点击进入后显示的图片链接地址
+							</item>
+							</Articles>
+							</xml> ";
 
-            if (!empty($event)) {
-                $msgType = "text";
-                $contentStr = "关注事件";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
-            }
+			if (!empty($event))
+			{
+				$msgType = "text";
+				$contentStr = "关注事件";
+				$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+				echo $resultStr;
+			}
 
-            $sql = "SELECT flag_id FROM user_flags WHERE from_user = '$fromUsername'";
-            $result = _select_data($sql);
-            while (!!$rows = mysql_fetch_array($result)) {
-                $user_flag = $rows[flag_id];
-            }
-            if (trim($keyword) <> $user_flag && is_numeric($keyword)) {
-                $user_flag = '';
-                $sql = "DELETE FROM user_flags WHERE from_user = '$fromUsername'";
-                _delete_data($sql);
-            }
+			$sql = "SELECT flag_id FROM user_flags WHERE from_user = '$fromUsername'";
+			$result = _select_data($sql);
+			while ($rows = mysql_fetch_array($result))
+			{
+				$user_flag = $rows[flag_id];
+			}
+			if (trim($keyword) != $user_flag && is_numeric($keyword))
+			{
+				$user_flag = '';
+				$sql = "DELETE FROM user_flags WHERE from_user = '$fromUsername'";
+				_delete_data($sql);
+			}
+			if (empty($user_flag))
+			{
+				if ($keyword == '1')
+				{
+					$sql = "INSERT INTO `user_flags` (`from_user`, `flag_id`) VALUE ('$fromUsername','1')";
+					_insert_data($sql);
+					$msgType = "text";
+					$contentStr = "Welcome to wechat world!";
+					$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+					echo $resultStr;
+				}
+				else
+				{
+					echo "Input something...";
+				}
+			}
 
-            if(!empty($keyword))
-            {
-                $msgType = "text";
-                $contentStr = "Welcome to wechat world!";
-                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                echo $resultStr;
-            }else{
-                echo "Input something...";
-            }
+		}
+		else
+		{
+			echo "";
+			exit;
+		}
+	}
 
-        }else {
-            echo "";
-            exit;
-        }
-    }
+	private function checkSignature()
+	{
+		// you must define TOKEN by yourself
+		if (!defined("TOKEN"))
+		{
+			throw new Exception('TOKEN is not defined!');
+		}
 
-    private function checkSignature()
-    {
-        // you must define TOKEN by yourself
-        if (!defined("TOKEN")) {
-            throw new Exception('TOKEN is not defined!');
-        }
+		$signature = $_GET["signature"];
+		$timestamp = $_GET["timestamp"];
+		$nonce = $_GET["nonce"];
 
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
+		$token = TOKEN;
+		$tmpArr = array($token, $timestamp, $nonce);
+		// use SORT_STRING rule
+		sort($tmpArr, SORT_STRING);
+		$tmpStr = implode( $tmpArr );
+		$tmpStr = sha1( $tmpStr );
 
-        $token = TOKEN;
-        $tmpArr = array($token, $timestamp, $nonce);
-        // use SORT_STRING rule
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
-
-        if( $tmpStr == $signature ){
-            return true;
-        }else{
-            return false;
-        }
-    }
+		if( $tmpStr == $signature )
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 }
 
 ?>
