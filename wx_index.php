58dd0b57bcf884d0930834b6cea49a2e693f4263
Fancy <jemringfan@foxmail.com>
2015-12-02 10:22:03 +0800

    guanzhu

--- wx_index.php
+++ wx_index.php
@@ -1,7 +1,7 @@
 <?php
 /**
-  * wechat php test
-  */
+ * wechat php test
+ */
 
 //define your token
 define("TOKEN", "weixin");
@@ -10,33 +10,33 @@ $wechatObj->responseMsg();
 
 class wechatCallbackapiTest
 {
-	public function valid()
+    public function valid()
     {
         $echoStr = $_GET["echostr"];
 
         //valid signature , option
         if($this->checkSignature()){
-        	echo $echoStr;
-        	exit;
+            echo $echoStr;
+            exit;
         }
     }
 
     public function responseMsg()
     {
-		//get post data, May be due to the different environments
-		$postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
+        //get post data, May be due to the different environments
+        $postStr = $GLOBALS["HTTP_RAW_POST_DATA"];
 
-      	//extract post data
-		if (!empty($postStr)){
-                /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
-                   the best way is to check the validity of xml by yourself */
-                libxml_disable_entity_loader(true);
-              	$postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
-                $fromUsername = $postObj->FromUserName;
-                $toUsername = $postObj->ToUserName;
-                $keyword = trim($postObj->Content);
-                $time = time();
-                $textTpl = "<xml>
+        //extract post data
+        if (!empty($postStr)){
+            /* libxml_disable_entity_loader is to prevent XML eXternal Entity Injection,
+               the best way is to check the validity of xml by yourself */
+            libxml_disable_entity_loader(true);
+            $postObj = simplexml_load_string($postStr, 'SimpleXMLElement', LIBXML_NOCDATA);
+            $fromUsername = $postObj->FromUserName;
+            $toUsername = $postObj->ToUserName;
+            $keyword = trim($postObj->Content);
+            $time = time();
+            $textTpl = "<xml>
 							<ToUserName><![CDATA[%s]]></ToUserName>
 							<FromUserName><![CDATA[%s]]></FromUserName>
 							<CreateTime>%s</CreateTime>
@@ -44,24 +44,47 @@ class wechatCallbackapiTest
 							<Content><![CDATA[%s]]></Content>
 							<FuncFlag>0</FuncFlag>
 							</xml>";
-				if(!empty( $keyword ))
-                {
-              		$msgType = "text";
-                	$contentStr = "Welcome to wechat world!";
-                	$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-                	echo $resultStr;
-                }else{
-                	echo "Input something...";
-                }
+            $imageTpl = "<xml>
+                            <ToUserName><![CDATA[%s]]></ToUserName>
+                            <FromUserName><![CDATA[%s]]></FromUserName>
+                            <CreateTime>%s</CreateTime>
+                            <MsgType><![CDATA[news]]></MsgType>//消息类型为news（图文）
+                            <ArticleCount>1</ArticleCount>//图文数量为1（单图文）
+                            <Articles>
+                            <item>//第一张图文消息
+                            <Title><![CDATA[%s]]></Title> //标题
+                            <Description><![CDATA[%s]]></Description>//描述
+                            <PicUrl><![CDATA[%s]]></PicUrl>//打开前的图片链接地址
+                            <Url><![CDATA[%s]]></Url>//点击进入后显示的图片链接地址
+                            </item>
+                            </Articles>
+                            </xml> ";
+            if (!empty($event)) {
+                /* $sql = "insert into sleep_man (`from_user`,`flag`) VALUES ('$fromUsername',0)";
+                 _insert_data($sql);*/
+                $msgType = "text";
+                $contentStr = "ddddd";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }
+            if(!empty( $keyword ))
+            {
+                $msgType = "text";
+                $contentStr = "Welcome to wechat world!";
+                $resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+                echo $resultStr;
+            }else{
+                echo "Input something...";
+            }
 
         }else {
-        	echo "";
-        	exit;
+            echo "";
+            exit;
         }
     }
 
-	private function checkSignature()
-	{
+    private function checkSignature()
+    {
         // you must define TOKEN by yourself
         if (!defined("TOKEN")) {
             throw new Exception('TOKEN is not defined!');
@@ -71,19 +94,19 @@ class wechatCallbackapiTest
         $timestamp = $_GET["timestamp"];
         $nonce = $_GET["nonce"];
 
-		$token = TOKEN;
-		$tmpArr = array($token, $timestamp, $nonce);
+        $token = TOKEN;
+        $tmpArr = array($token, $timestamp, $nonce);
         // use SORT_STRING rule
-		sort($tmpArr, SORT_STRING);
-		$tmpStr = implode( $tmpArr );
-		$tmpStr = sha1( $tmpStr );
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
 
-		if( $tmpStr == $signature ){
-			return true;
-		}else{
-			return false;
-		}
-	}
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
+    }
 }
 
 ?>
