cb25b619287b97fb261ac48e2e218097ee54e1df
Fancy <jemringfan@foxmail.com>
2015-12-02 19:20:20 +0800

    解绑

---
+++ bd/bangdingmodel.php
@@ -0,0 +1,38 @@
+<?php
+require_once '../sql.php';
+
+function jiebang()
+{
+	$sql = "SELECT * FROM `user_bangding` WHERE `from_user` = '$fromUsername'";
+	$res = _select_data($sql);
+	if(!empty($res))
+	{
+		$sql = "DELETE FROM `user_bangding` WHERE `from_user` = '$fromUsername'";
+		$res = _delete_data($sql);
+
+		if($res == 1)
+		{
+			$msgType = "text";
+			$contentStr = '解绑工号成功~';
+			$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+			echo $resultStr;
+		}
+		else
+		{
+			$msgType = "text";
+			$contentStr = '解绑工号失败~';
+			$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+			echo $resultStr;
+		}
+
+	}
+	else
+	{
+		$msgType = "text";
+		$contentStr = '还未绑定工号~';
+		$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+		echo $resultStr;
+	}
+}
+
+?>
--- wx_index.php
+++ wx_index.php
@@ -91,36 +91,7 @@ class wechatCallbackapiTest
 				}
 				else if ($keyword == '#' || $keyword == '解绑' || $keyword == '解除绑定')
 				{
-					$sql = "SELECT * FROM `user_bangding` WHERE `from_user` = '$fromUsername'";
-					$res = _select_data($sql);
-					if(!empty($res))
-					{
-						$sql1 = "DELETE FROM `user_bangding` WHERE `from_user` = '$fromUsername'";
-						$res1 = _delete_data($sql1);
-
-						if($res1 == 1)
-						{
-							$msgType = "text";
-							$contentStr = '解绑工号成功~';
-							$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-							echo $resultStr;
-						}
-						else
-						{
-							$msgType = "text";
-							$contentStr = '解绑工号失败~';
-							$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-							echo $resultStr;
-						}
-
-					}
-					else
-					{
-						$msgType = "text";
-						$contentStr = '还未绑定工号~';
-						$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-						echo $resultStr;
-					}
+					jiebang();
 				}
 				else
 				{
