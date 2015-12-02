b2cf1939b049b7845ca09b8143efbe098f55dd41
Fancy <jemringfan@foxmail.com>
2015-12-02 19:08:58 +0800

    解绑

--- wx_index.php
+++ wx_index.php
@@ -91,30 +91,30 @@ class wechatCallbackapiTest
 				}
 				else if ($keyword == '#' || $keyword == '解绑' || $keyword == '解除绑定')
 				{
-					$sql = "SELECT * FROM `user_bangding` WHERE `from_user` = '$fromUsername'";
-					$res = _select_data($sql);
-					if(!empty($res))
-					{
-						$sql1 = "DELETE * FROM `user_bangding` WHERE `from_user` = '$fromUsername'";
-						$res1 = _select_data($sql1);
-
-						if($res1 == 1)
-						{
-							$msgType = "text";
-							$contentStr = '解绑工号成功~';
-							$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-							echo $resultStr;
-						}
-						else
-						{
-							$msgType = "text";
-							$contentStr = '解绑工号失败~';
-							$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
-							echo $resultStr;
-						}
-
-					}
-					else
+//					$sql = "SELECT * FROM `user_bangding` WHERE `from_user` = '$fromUsername'";
+//					$res = _select_data($sql);
+//					if(!empty($res))
+//					{
+//						$sql1 = "DELETE * FROM `user_bangding` WHERE `from_user` = '$fromUsername'";
+//						$res1 = _select_data($sql1);
+//
+//						if($res1 == 1)
+//						{
+//							$msgType = "text";
+//							$contentStr = '解绑工号成功~';
+//							$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+//							echo $resultStr;
+//						}
+//						else
+//						{
+//							$msgType = "text";
+//							$contentStr = '解绑工号失败~';
+//							$resultStr = sprintf($textTpl, $fromUsername, $toUsername, $time, $msgType, $contentStr);
+//							echo $resultStr;
+//						}
+//
+//					}
+//					else
 					{
 						$msgType = "text";
 						$contentStr = '还未绑定工号~';
