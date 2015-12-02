0bdeb78c03891381a1e50154ced80bc7d26d1963
Fancy <jemringfan@foxmail.com>
2015-12-02 19:54:56 +0800

    解绑

--- wx_index.php
+++ wx_index.php
@@ -93,7 +93,7 @@ class wechatCallbackapiTest
 				{
 					$sql = "SELECT * FROM `user_bangding` WHERE `from_user` = '$fromUsername'";
 					$res = _select_data($sql);
-					if(!empty($res['uid']))
+					if($res['uid'])
 					{
 						$sql = "DELETE FROM `user_bangding` WHERE `from_user` = '$fromUsername'";
 						$res = _delete_data($sql);
