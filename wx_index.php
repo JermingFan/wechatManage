3afaa06df26b1ddab90cbbd3dafbcb6c49476615
Fancy <jemringfan@foxmail.com>
2015-12-09 10:06:43 +0800

    查看用户

--- wx_index.php
+++ wx_index.php
@@ -301,7 +301,7 @@ class wechatCallbackapiTest
 							}
 							$title = "工号---姓名---职务---状态";
 							$PicUrl = "";
-							$Description = $rows['uid'].'-----'.$rows['name'].'-----'.$rows['job'].'-----'.$state;
+							$Description = ''//$rows['uid'].'-----'.$rows['name'].'-----'.$rows['job'].'-----'.$state;
 							$Url = "";
 							$resultStr = sprintf($imageTpl, $fromUsername, $toUsername, $time, $title, $Description, $PicUrl, $Url);
 							echo $resultStr;
