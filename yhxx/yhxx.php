a84bb1c7b2714f712b74c435489f69605a5a73fc
Fancy <jemringfan@foxmail.com>
2015-12-09 11:02:07 +0800

    查看用户

--- yhxx/yhxx.php
+++ yhxx/yhxx.php
@@ -6,26 +6,19 @@ function _yhxx()
 	$sql = "SELECT * FROM `user_info`";
 	$res = _select_data($sql);
 	$arr = array();
-	while ($rows = mysql_fetch_array($res)) {
-		if ($rows['state'] == 1) {
+	while ($rows = mysql_fetch_array($res))
+	{
+		if ($rows['state'] == 1)
+		{
 			$state = '在职';
 		}
-		else {
+		else
+		{
 			$state = '其他';
 		}
-
-//		$arr = array(
-//			'uid' => $rows['uid'],
-//			'name' => $rows['name'],
-//			'job' => $rows['job'],
-//			'state' => $state,
-//		);
-//
-//		foreach ($arr as $a) {
-
-			$v=''.$rows['uid'].'';
-		}
-		return $v;
+		$v=''.'2222222'.'';
+	}
+	return $v;
 
 }
 ?>
