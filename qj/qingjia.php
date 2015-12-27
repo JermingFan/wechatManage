1c92de0c1500249c6e53d382e4e2bd32f1909b10
Fancy <jemringfan@foxmail.com>
2015-12-27 15:36:27 +0800

    shit

--- qj/qingjia.php
+++ qj/qingjia.php
@@ -34,7 +34,7 @@ $res = _select_data($sql);
 $rows = mysql_fetch_array($res);
 function qingjia($fromUsername, $uid, $long, $info)
 { $a = time();
-    $b = date(time());
+    $b = date('Y-m-d', time());
     var_dump($b);die;
 //    $sql = "INSERT INTO `user_qingjia` (`from_user`, `uid`, `time`, `endtime`, `info`) values ('$fromUsername', '$uid', '$time', '$endtime', '$info')";
 //    $res = _insert_data($sql);
