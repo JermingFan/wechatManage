3694c09f5157d14faf3a4b40cdcadcbca8dd6a4f
Fancy <jemringfan@foxmail.com>
2015-12-30 16:04:49 +0800

    qingjia

--- bd/xiugai.php
+++ bd/xiugai.php
@@ -37,7 +37,7 @@ if(isset($_POST["submit"]))
     exit();
 }
 $getuid = $_GET['uid'];
-$sql = "SELECT * FROM `user_bangding ` WHERE `uid` = '$getuid'";
+$sql = "SELECT * FROM `user_bangding` WHERE `uid` = '$getuid'";
 $res = _select_data($sql);
 $rows = mysql_fetch_array($res);
 ?>
